13d90c3c602f48570c3222de177781444de36005
Sani4-dev <sd_mai@mail.ru>
2020-10-19 20:56:45 +0300

    update20201019

---
+++ Dorokhin_Sergey_Task08/Task1/DynamicArray.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Task1
+{
+    public class DynamicArray<T> where T : class, new()
+    {
+        protected const int lengthArrayDefault = 8;
+        
+        protected T[] _arrayOfT;
+        protected int _indexElementOfArrayToAdditing = 0;
+
+        public DynamicArray()
+        {
+            _arrayOfT = new T[lengthArrayDefault];
+        }
+
+        public DynamicArray(int lengthOfArray)
+        {
+            if (lengthOfArray == 0)
+            {
+                throw new Exception("Аргумент \"lengthOfArray\" должен быть больше 0!");
+            }
+            _arrayOfT = new T[lengthOfArray];
+        }
+
+        public DynamicArray(T[] arrayOfT)
+        {
+            _arrayOfT = arrayOfT ?? throw new Exception("Аргумент конструктора не должен быть равен \"NULL\"");
+            foreach (var item in arrayOfT)
+            {
+                if (item == null)
+                {
+                    throw new Exception("Передаваемый массив должен быть полностью заполнен!");
+                }
+            }
+            _indexElementOfArrayToAdditing += arrayOfT.Length;
+        }
+
+        public void Add(T objectToAdd)
+        {
+            if (_indexElementOfArrayToAdditing > _arrayOfT.Length - 1)
+            {
+                Array.Resize(ref _arrayOfT, _arrayOfT.Length * 2);
+            }
+
+            _arrayOfT[_indexElementOfArrayToAdditing] = objectToAdd ?? throw new Exception("Аргумент метода не должен быть равен \"NULL\"");
+            _indexElementOfArrayToAdditing++;
+        }
+
+        public void AddRange(T[] arrayOfT)
+        {
+            if (arrayOfT == null)
+            {
+                throw new Exception("Аргумент метода не должен быть равен \"NULL\"");
+            }
+
+            foreach (var item in arrayOfT)
+            {
+                if (item == null)
+                {
+                    throw new Exception("Передаваемый массив должен быть полность заполнен!");
+                }
+            }
+
+            int quantityEmptyElementsOfArray = _arrayOfT.Length - _indexElementOfArrayToAdditing;
+
+            if (arrayOfT.Length > quantityEmptyElementsOfArray)
+            {
+                Array.Resize(ref _arrayOfT, arrayOfT.Length - quantityEmptyElementsOfArray + _arrayOfT.Length);
+            }
+
+            arrayOfT.CopyTo(_arrayOfT, _indexElementOfArrayToAdditing);
+            _indexElementOfArrayToAdditing += arrayOfT.Length;
+        }
+
+        public bool Remove(int indexOf)
+        {
+            if (_indexElementOfArrayToAdditing == 0 || indexOf > _indexElementOfArrayToAdditing - 1)
+            {
+                return false;
+            }
+            else
+            {
+                if (indexOf == _indexElementOfArrayToAdditing - 1)
+                {
+                    _indexElementOfArrayToAdditing--;
+                }
+                else if(indexOf < _indexElementOfArrayToAdditing - 1)
+                {
+                    T[] arrayHelper = new T[_indexElementOfArrayToAdditing - indexOf];
+
+                    Array.ConstrainedCopy(_arrayOfT, indexOf + 1, arrayHelper, 0, _indexElementOfArrayToAdditing - 1 -indexOf);
+                    arrayHelper.CopyTo(_arrayOfT, indexOf);
+                    _indexElementOfArrayToAdditing--;
+                }
+
+                return true;
+            }
+        }
+
+        public void Insert(int indexTo, T objectToInsert)
+        {
+            if (objectToInsert == null)
+            {
+                throw new Exception("Передаваемый объект не должен быть равен \"NULL\"!");
+            }
+            
+            if (indexTo > _indexElementOfArrayToAdditing - 1)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            Array.Resize(ref _arrayOfT, _indexElementOfArrayToAdditing + 1);
+
+            if (indexTo == _indexElementOfArrayToAdditing - 1)
+            {
+                _arrayOfT[_indexElementOfArrayToAdditing] = objectToInsert;
+            }
+            else
+            {
+                T[] arrayHelper = new T[_indexElementOfArrayToAdditing - indexTo];
+
+                Array.ConstrainedCopy(_arrayOfT, indexTo, arrayHelper, 0, _indexElementOfArrayToAdditing - indexTo);
+                arrayHelper.CopyTo(_arrayOfT, indexTo + 1);
+                _arrayOfT[indexTo] = objectToInsert;
+            }
+
+            _indexElementOfArrayToAdditing++;
+        }
+
+        public int Length
+        {
+            get
+            {
+                return _indexElementOfArrayToAdditing;
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return _arrayOfT.Length;
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index > _arrayOfT.Length - 1)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                return _arrayOfT[index];
+            }
+
+            set
+            {
+                if (index > _arrayOfT.Length - 1)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                _arrayOfT[index] = value;
+            }
+        }
+    }
+}
---
+++ Dorokhin_Sergey_Task08/Task1/Program.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class MyObject
+    {
+        public MyObject()
+        {
+        }
+
+        public int Number { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Проверка конструктора без параметров:");
+
+            var dynamicArray = new DynamicArray<object>();
+
+            Console.WriteLine($"Length: {dynamicArray.Length}");
+            Console.WriteLine($"Capacity: {dynamicArray.Capacity}");
+            Console.WriteLine("Содержимое массива:");
+            for (int i = 0; i < dynamicArray.Length; i++)
+            {
+                Console.WriteLine(dynamicArray[i] ?? "NULL");
+            }
+
+            Console.WriteLine("Проверка конструктора с параметром, задающим размер массива:");
+            Console.WriteLine("Создается пустой массив размером 5:");
+
+            dynamicArray = new DynamicArray<object>(5);
+
+            Console.WriteLine($"Length: {dynamicArray.Length}");
+            Console.WriteLine($"Capacity: {dynamicArray.Capacity}");
+            Console.WriteLine("Содержимое массива:");
+            for (int i = 0; i < dynamicArray.Length; i++)
+            {
+                Console.WriteLine(dynamicArray[i] ?? "NULL");
+            }
+
+            Console.WriteLine("Проверка конструктора с параметром, задающим массив содержащий 3 элемента \"Object\":");
+
+            dynamicArray = new DynamicArray<object>(new object[] { new object(), new object(), new object() });
+
+            Console.WriteLine($"Length: {dynamicArray.Length}");
+            Console.WriteLine($"Capacity: {dynamicArray.Capacity}");
+            Console.WriteLine("Содержимое массива:");
+            for (int i = 0; i < dynamicArray.Length; i++)
+            {
+                Console.WriteLine(dynamicArray[i] ?? "NULL");
+            }
+
+            Console.WriteLine("Проверка метода \"Add\". Добавление элемента к массиву с \"Length\" = 3 и \"Capacity\" = 3:");
+
+            dynamicArray.Add(new object());
+            
+            Console.WriteLine($"Length: {dynamicArray.Length}");
+            Console.WriteLine($"Capacity: {dynamicArray.Capacity}");
+            Console.WriteLine("Содержимое массива:");
+            for (int i = 0; i < dynamicArray.Length; i++)
+            {
+                Console.WriteLine(dynamicArray[i] ?? "NULL");
+            }
+
+            Console.WriteLine("Проверка метода \"Add\". Добавление элемента к массиву с \"Length\" = 4 и \"Capacity\" = 6:");
+
+            dynamicArray.Add(new object());
+
+            Console.WriteLine($"Length: {dynamicArray.Length}");
+            Console.WriteLine($"Capacity: {dynamicArray.Capacity}");
+            Console.WriteLine("Содержимое массива:");
+            for (int i = 0; i < dynamicArray.Length; i++)
+            {
+                Console.WriteLine(dynamicArray[i] ?? "NULL");
+            }
+
+            Console.WriteLine("Проверка метода \"AddRange\". Добавление массива с 2-мя элементами" +
+                " к массиву с \"Length\" = 0 и \"Capacity\" = 3:");
+
+            dynamicArray = new DynamicArray<object>(3);
+
+            dynamicArray.AddRange(new object[] { new object(), new object() });
+
+            Console.WriteLine($"Length: {dynamicArray.Length}");
+            Console.WriteLine($"Capacity: {dynamicArray.Capacity}");
+            Console.WriteLine("Содержимое массива:");
+            for (int i = 0; i < dynamicArray.Length; i++)
+            {
+                Console.WriteLine(dynamicArray[i] ?? "NULL");
+            }
+
+            Console.WriteLine("Проверка метода \"AddRange\". Добавление массива с 3-мя элементами" +
+                " к массиву с \"Length\" = 2 и \"Capacity\" = 3:");
+
+            dynamicArray.AddRange(new object[] { new object(), new object(), new object() });
+
+            Console.WriteLine($"Length: {dynamicArray.Length}");
+            Console.WriteLine($"Capacity: {dynamicArray.Capacity}");
+            Console.WriteLine("Содержимое массива:");
+            for (int i = 0; i < dynamicArray.Length; i++)
+            {
+                Console.WriteLine(dynamicArray[i] ?? "NULL");
+            }
+
+            Console.WriteLine("Проверка метода \"Remove\". Удаление элемента если массив пуст." +
+                " Исходный массив с \"Length\" = 0 и \"Capacity\" = 3:");
+
+            dynamicArray = new DynamicArray<object>(3);
+
+            Console.WriteLine($"Результат выполнения метода: {dynamicArray.Remove(0)}");
+
+            Console.WriteLine($"Length: {dynamicArray.Length}");
+            Console.WriteLine($"Capacity: {dynamicArray.Capacity}");
+            Console.WriteLine("Содержимое массива:");
+            for (int i = 0; i < dynamicArray.Length; i++)
+            {
+                Console.WriteLine(dynamicArray[i] ?? "NULL");
+            }
+
+            Console.WriteLine("Проверка метода \"Remove\". Удаление элемента с индексом 3." +
+                " Исходный массив с \"Length\" = 3 и \"Capacity\" = 6:");
+
+            dynamicArray = new DynamicArray<object>(6);
+
+            dynamicArray.Add(new object());
+            dynamicArray.Add(new object());
+            dynamicArray.Add(new object());
+
+            Console.WriteLine($"Результат выполнения метода: {dynamicArray.Remove(3)}");
+
+            Console.WriteLine($"Length: {dynamicArray.Length}");
+            Console.WriteLine($"Capacity: {dynamicArray.Capacity}");
+            Console.WriteLine("Содержимое массива:");
+            for (int i = 0; i < dynamicArray.Length; i++)
+            {
+                Console.WriteLine(dynamicArray[i] ?? "NULL");
+            }
+
+            Console.WriteLine("Проверка метода \"Remove\". Удаление элемента с индексом 2." +
+                " Исходный массив с \"Length\" = 3 и \"Capacity\" = 6:");
+
+            Console.WriteLine($"Результат выполнения метода: {dynamicArray.Remove(2)}");
+
+            Console.WriteLine($"Length: {dynamicArray.Length}");
+            Console.WriteLine($"Capacity: {dynamicArray.Capacity}");
+            Console.WriteLine("Содержимое массива:");
+            for (int i = 0; i < dynamicArray.Length; i++)
+            {
+                Console.WriteLine(dynamicArray[i] ?? "NULL");
+            }
+
+            Console.WriteLine("Проверка метода \"Remove\". Удаление элемента с индексом 1." +
+                " Исходный массив с \"Length\" = 3 и \"Capacity\" = 6:");
+
+            dynamicArray = new DynamicArray<object>(6);
+
+            dynamicArray.Add(new object());
+            dynamicArray.Add(new object());
+            dynamicArray.Add(new object());
+
+            Console.WriteLine($"Результат выполнения метода: {dynamicArray.Remove(1)}");
+
+            Console.WriteLine($"Length: {dynamicArray.Length}");
+            Console.WriteLine($"Capacity: {dynamicArray.Capacity}");
+            Console.WriteLine("Содержимое массива:");
+            for (int i = 0; i < dynamicArray.Length; i++)
+            {
+                Console.WriteLine(dynamicArray[i] ?? "NULL");
+            }
+
+            /*Console.WriteLine("Проверка метода \"Insert\". Вставка элемента по индексу 5." +
+                " Исходный массив с \"Length\" = 5 и \"Capacity\" = 5:");
+
+            dynamicArray = new DynamicArray<object>(new object[] { new object(), new object(), new object(), new object(), new object() });
+
+            dynamicArray.Insert(5, new object());
+
+            Console.WriteLine($"Length: {dynamicArray.Length}");
+            Console.WriteLine($"Capacity: {dynamicArray.Capacity}");
+            Console.WriteLine("Содержимое массива:");
+            for (int i = 0; i < dynamicArray.Length; i++)
+            {
+                Console.WriteLine(dynamicArray[i] ?? "NULL");
+            }*/
+
+            Console.WriteLine("Проверка метода \"Insert\". Вставка элемента по индексу 4." +
+                " Исходный массив с \"Length\" = 5 и \"Capacity\" = 5:");
+
+            dynamicArray = new DynamicArray<object>(new object[] { new object(), new object(), new object(), new object(), new object() });
+
+            dynamicArray.Insert(4, new object());
+
+            Console.WriteLine($"Length: {dynamicArray.Length}");
+            Console.WriteLine($"Capacity: {dynamicArray.Capacity}");
+            Console.WriteLine("Содержимое массива:");
+            for (int i = 0; i < dynamicArray.Length; i++)
+            {
+                Console.WriteLine(dynamicArray[i] ?? "NULL");
+            }
+
+            Console.WriteLine("Проверка метода \"Insert\". Вставка элемента по индексу 0." +
+                " Исходный массив с \"Length\" = 6 и \"Capacity\" = 6:");
+
+            dynamicArray.Insert(0, new object());
+
+            Console.WriteLine($"Length: {dynamicArray.Length}");
+            Console.WriteLine($"Capacity: {dynamicArray.Capacity}");
+            Console.WriteLine("Содержимое массива:");
+            for (int i = 0; i < dynamicArray.Length; i++)
+            {
+                Console.WriteLine(dynamicArray[i] ?? "NULL");
+            }
+
+            
+            Console.ReadKey();
+        }
+    }
+}
